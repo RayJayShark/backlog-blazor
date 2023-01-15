50601e0c27194109babb03158fd35214324f6611
Joshua Ray <josh.ray7575@yahoo.com>
2023-01-15 23:42:15 +0000

    LOG-BLAZOR-T-12/deployment-build Remove ./
    Add ./
    
    Debugging tailwindcss execution
    Download tailwind cli
    Fix bad image name
    Another fix
    Fix parse error
    PWD test
    
    Co-authored-by: Joshua Ray <superdog1695@yahoo.com>
    
    Merge-request: LOG-BLAZOR-MR-3
    Merged-by: Joshua Ray <josh.ray7575@yahoo.com>

---
+++ .space.kts
@@ -0,0 +1,24 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Build") {
+	container(displayName = "Build Instance", image = "mcr.microsoft.com/dotnet/sdk:7.0.102-jammy-amd64") {
+
+    	shellScript {
+  			content = """
+				cd Backlog-Blazor
+    			wget -O tailwindcss https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
+       			ls
+          		chmod +x tailwindcss
+    			dotnet publish -o build
+     		"""
+      	}
+    }
+
+    startOn {
+    	gitPush { enabled = true }
+    }
+}
