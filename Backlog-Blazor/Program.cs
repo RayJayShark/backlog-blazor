94be38d317fffc032978d38c1d099f85497abd62
Joshua Ray <josh.ray7575@yahoo.com>
2023-01-16 22:50:00 +0000

    Basic navbar Basic dropdown hamburger nav menu
    Added hover icon for profile picture
    
    Cleaned up some CSS
    
    Basic navbar
    
    Added hamburger icon to navbar
    
    Change namespace of project
    
    Purge missed CSS
    
    Merge-request: LOG-BLAZOR-MR-4
    Merged-by: Joshua Ray <josh.ray7575@yahoo.com>

--- Backlog-Blazor/Program.cs
+++ Backlog-Blazor/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Backlog_Blazor;
+using BacklogBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
