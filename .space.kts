24d3002d4c7621fe612fa5fc3b70dc897a64d489
Joshua Ray <josh.ray7575@yahoo.com>
2023-03-20 04:22:26 +0000

    Fix deployment script to build solution instead of project

--- .space.kts
+++ .space.kts
@@ -13,6 +13,7 @@ job("Build") {
     			wget -O tailwindcss https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
        			ls
           		chmod +x tailwindcss
+            	cd ..
     			dotnet publish -o build
      		"""
       	}
